a55bfe6433f432bd6dbe15ee9b3aafb8d9da0ff5
ResEl Admins <root@dynasty.adm.maisel.enst-bretagne.fr>
2015-06-12 15:38:50 +0200

    [ResEl.Tools] add som doctest

--- src/resel/tools/__init__.py
+++ src/resel/tools/__init__.py
@@ -9,7 +9,7 @@ Tools for python Resel scripts
 """
 
 from network import is_valid_ip,is_valid_ip_main, ip_in_subnet, ip_in_subnet_main
-
+from utils import read_file
 __docformat__ = 'restructuredtext en'
 
 # vim:set et sts=4 ts=4 tw=80:
