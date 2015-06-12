dd2c0896ad0d0789bb60596572bb988e8e29009c
ResEl Admins <root@dynasty.adm.maisel.enst-bretagne.fr>
2015-06-12 16:55:58 +0200

    [ResEl.Tools] Import add_2_dhcpd function in tools

--- src/resel/tools/__init__.py
+++ src/resel/tools/__init__.py
@@ -10,6 +10,8 @@ Tools for python Resel scripts
 
 from network import is_valid_ip,is_valid_ip_main, ip_in_subnet, ip_in_subnet_main
 from utils import read_file
+from dhcpd import add_2_dhcpd 
+
 __docformat__ = 'restructuredtext en'
 
 # vim:set et sts=4 ts=4 tw=80:
